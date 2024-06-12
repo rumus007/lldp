2e029782d6bd465a6c32e386698579c1bfb7f143
rbajracharya <rumus.3353@gmail.com>
2024-06-12 10:57:00 -0700

    Command line functionality for lldpcli

--- index.py
+++ index.py
@@ -29,55 +29,82 @@ def run_lldpcli_command(command):
         print(f"Error Output: {e.stderr}")
         return None
 
-def create_dict(data):
+def create_dict(chassis, neighbor):
     """
     Creates a dictionary of the information provided for storage
 
     Parameters:
-    - data: JSON data from the CLI command
+    - chassis: JSON data for chassis from the CLI command
+    - neighbor: JSON data for neighbor from the CLI command
 
     Returns:
     - The the dictionary value
     """
-    temp_dict = {
-        'node_mac': '',
-        'sys_name': '',
-        'sys_description': '',
-        'mgmt_ip': '',
-        'capability': {
-            'Bridge': False,
-            'Router': False,
-            'Wlan': False,
-            'Station': False
-        },
-    }
+    info = { "chassis": None, "neighbor": [] }
 
     try:
-        #code
-        checked_data = deep_get(data,['local-chassis',0,'chassis',0])
+        #code for chassis information
+        temp_dict = {}
+        checked_data = deep_get(chassis,['local-chassis',0,'chassis',0])
 
         if checked_data:
-            temp_dict['node_mac'] = deep_get(checked_data,['id',0,'value'])
+            temp_dict['node_id'] = deep_get(checked_data,['id',0,'value'])
+            temp_dict['node_id_type'] = deep_get(checked_data,['id',0,'type'])
             temp_dict['sys_name'] = deep_get(checked_data,['name',0,'value'])
             temp_dict['sys_description'] = deep_get(checked_data,['descr',0,'value'])
             temp_dict['mgmt_ip'] = deep_get(checked_data,['mgmt-ip',0,'value'])
+            temp_dict['capability'] = {}
             
             if checked_data['capability']:
                 for capabilities in checked_data['capability']:
                     temp_dict['capability'][capabilities['type']] = capabilities['enabled']
-    except Exception as Err:
-        #code
-        print(type(Err))
+        
+        info['chassis'] = temp_dict
+    
+        # Code for neighbor information
+        checked_data = deep_get(neighbor, ['lldp',0,'interface'])
 
-    return temp_dict
+        if checked_data:
+            for neighbor in checked_data:
+                temp_dict = {}
+                chassis_info = deep_get(neighbor, ['chassis', 0])
+                port_info = deep_get(neighbor, ['port', 0])
+                temp_dict['neigbor_id'] = deep_get(chassis_info, ['id', 0, 'value'])
+                temp_dict['neigbor_id_type'] = deep_get(chassis_info, ['id', 0, 'type'])
+                temp_dict['name'] = deep_get(chassis_info, ['name', 0, 'value'])
+                temp_dict['mgmt_ip'] = deep_get(chassis_info, ['mgmt-ip', 0, 'value'])
+                temp_dict['port_id_type'] = deep_get(port_info, ['id', 0, 'type'])
+                temp_dict['port_id'] = deep_get(port_info, ['id', 0, 'value'])
+                temp_dict['port_description'] = deep_get(port_info, ['descr', 0, 'value'])
+                temp_dict['port_ttl'] = deep_get(port_info, ['ttl', 0, 'value'])
+                temp_dict['capability'] = {}
+
+                if chassis_info['capability']:
+                    for capabilities in chassis_info['capability']:
+                        temp_dict['capability'][capabilities['type']] = capabilities['enabled']
+
+                info['neighbor'].append(temp_dict)
+
+        return info
+
+    except Exception as e:
+        #code
+        print(type(e))
+        print(f"Error running command: {e}")
 
 # Command to run local chassis information
-command = "sudo lldpcli -f json0 show chassis"
+command_chassis = "sudo lldpcli -f json0 show chassis"
+
+# Command to run neighbor information
+command_neighbors = "sudo lldpcli -f json0 show neighbors"
 
 # Run the command and get the output
-output = run_lldpcli_command(command)
+output_chassis = run_lldpcli_command(command_chassis)
+output_neighbors = run_lldpcli_command(command_neighbors)
 
-if output:
-    json_output = json.loads(output)
-    test = create_dict(json_output)
+
+if output_chassis:
+    json_output_chassis = json.loads(output_chassis)
+    json_output_neighbors = json.loads(output_neighbors)
+    test = create_dict(json_output_chassis, json_output_neighbors)
     print(test)
