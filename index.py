3dbf4577ed76953e8eb2b13769dcd8a4ae3c0174
rbajracharya <rumus.3353@gmail.com>
2024-06-17 15:55:45 -0700

    Refactored to class based approach

--- index.py
+++ index.py
@@ -3,159 +3,165 @@ import json
 import http.client
 from helper import *
 
-def run_lldpcli_command(command):
+class LldpCollector:
     """
-    Runs the lldpcli command to get self and neighbor information
-
-    Parameters:
-    - command: The cli command to be runned
-
-    Returns:
-    - The value if found, otherwise None.
+    This class collects LLDP information and interacts with an API.
     """
-    try:
-        # Run the command and capture the output
-        result = subprocess.run(
-            command, 
-            shell=True,
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
-        return result.stdout
-    except subprocess.CalledProcessError as e:
-        print(f"Error running command: {e}")
-        print(f"Output: {e.output}")
-        print(f"Error Output: {e.stderr}")
-        return None
 
-def create_dict(chassis, neighbor):
-    """
-    Creates a dictionary of the information provided for storage
+    def __init__(self, server_url: str, api_endpoint: str):
+        self.server_url = server_url
+        self.api_endpoint = api_endpoint
+        self.command_chassis = "sudo lldpcli -f json0 show chassis"
+        self.command_neighbors = "sudo lldpcli -f json0 show neighbors"
 
-    Parameters:
-    - chassis: JSON data for chassis from the CLI command
-    - neighbor: JSON data for neighbor from the CLI command
+    def run_lldpcli_command(self, command):
+        """
+        Runs the lldpcli command and captures the output.
 
-    Returns:
-    - The the dictionary value
-    """
-    info = { "chassis": None, "neighbor": [] }
+        Args:
+            command: The command to be executed.
 
-    try:
-        #code for chassis information
-        temp_dict = {}
-        checked_data = deep_get(chassis,['local-chassis',0,'chassis',0])
+        Returns:
+            The output of the command as a string, or None if an error occurs.
+        """
+        try:
+            result = subprocess.run(
+                command,
+                shell=True,
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True
+            )
+            return result.stdout
+        except subprocess.CalledProcessError as e:
+            print(f"Error running command: {e}")
+            print(f"Output: {e.output}")
+            print(f"Error Output: {e.stderr}")
+            return None
 
-        if checked_data:
-            temp_dict['node_id'] = deep_get(checked_data,['id',0,'value'])
-            temp_dict['node_id_type'] = deep_get(checked_data,['id',0,'type'])
-            temp_dict['sys_name'] = deep_get(checked_data,['name',0,'value'])
-            temp_dict['sys_description'] = deep_get(checked_data,['descr',0,'value'])
-            temp_dict['mgmt_ip'] = deep_get(checked_data,['mgmt-ip',0,'value'])
-            temp_dict['capability'] = {
-                'Bridge': False, 
-                'Router': False, 
-                'Wlan': False, 
-                'Station': False
-            }
-            
-            if ("capability" in checked_data) and checked_data['capability']:
-                for capabilities in checked_data['capability']:
-                    temp_dict['capability'][capabilities['type']] = capabilities['enabled']
-        
-        info['chassis'] = temp_dict
-    
-        # Code for neighbor information
-        checked_data = deep_get(neighbor, ['lldp',0,'interface'])
+    def create_dict(self, chassis, neighbor):
+        """
+        Creates a dictionary with structured information from lldp output.
 
-        if checked_data:
-            for neighbor in checked_data:
-                temp_dict = {}
-                chassis_info = deep_get(neighbor, ['chassis', 0])
-                port_info = deep_get(neighbor, ['port', 0])
-                temp_dict['neigbor_id'] = deep_get(chassis_info, ['id', 0, 'value'])
-                temp_dict['neigbor_id_type'] = deep_get(chassis_info, ['id', 0, 'type'])
-                temp_dict['name'] = deep_get(chassis_info, ['name', 0, 'value'])
-                temp_dict['mgmt_ip'] = deep_get(chassis_info, ['mgmt-ip', 0, 'value'])
-                temp_dict['port_id_type'] = deep_get(port_info, ['id', 0, 'type'])
-                temp_dict['port_id'] = deep_get(port_info, ['id', 0, 'value'])
-                temp_dict['port_description'] = deep_get(port_info, ['descr', 0, 'value'])
-                temp_dict['port_ttl'] = deep_get(port_info, ['ttl', 0, 'value'])
+        Args:
+            chassis_data: JSON data for the local chassis.
+            neighbor_data: JSON data for neighboring devices.
+
+        Returns:
+            A dictionary containing chassis and neighbor information.
+        """
+        info = {"chassis": None, "neighbor": []}
+
+        try:
+            #code for chassis information
+            temp_dict = {}
+            checked_data = deep_get(chassis,['local-chassis',0,'chassis',0])
+
+            if checked_data:
+                temp_dict['node_id'] = deep_get(checked_data,['id',0,'value'])
+                temp_dict['node_id_type'] = deep_get(checked_data,['id',0,'type'])
+                temp_dict['sys_name'] = deep_get(checked_data,['name',0,'value'])
+                temp_dict['sys_description'] = deep_get(checked_data,['descr',0,'value'])
+                temp_dict['mgmt_ip'] = deep_get(checked_data,['mgmt-ip',0,'value'])
                 temp_dict['capability'] = {
                     'Bridge': False, 
                     'Router': False, 
                     'Wlan': False, 
                     'Station': False
                 }
-
-                if ("capability" in chassis_info) and chassis_info['capability']:
-                    for capabilities in chassis_info['capability']:
+                
+                if ("capability" in checked_data) and checked_data['capability']:
+                    for capabilities in checked_data['capability']:
                         temp_dict['capability'][capabilities['type']] = capabilities['enabled']
+            
+                info['chassis'] = temp_dict
+        
+            # Code for neighbor information
+            checked_data = deep_get(neighbor, ['lldp',0,'interface'])
 
-                info['neighbor'].append(temp_dict)
+            if checked_data:
+                for neighbor in checked_data:
+                    temp_dict = {}
+                    chassis_info = deep_get(neighbor, ['chassis', 0])
+                    port_info = deep_get(neighbor, ['port', 0])
+                    temp_dict['neigbor_id'] = deep_get(chassis_info, ['id', 0, 'value'])
+                    temp_dict['neigbor_id_type'] = deep_get(chassis_info, ['id', 0, 'type'])
+                    temp_dict['name'] = deep_get(chassis_info, ['name', 0, 'value'])
+                    temp_dict['mgmt_ip'] = deep_get(chassis_info, ['mgmt-ip', 0, 'value'])
+                    temp_dict['port_id_type'] = deep_get(port_info, ['id', 0, 'type'])
+                    temp_dict['port_id'] = deep_get(port_info, ['id', 0, 'value'])
+                    temp_dict['port_description'] = deep_get(port_info, ['descr', 0, 'value'])
+                    temp_dict['port_ttl'] = deep_get(port_info, ['ttl', 0, 'value'])
+                    temp_dict['capability'] = {
+                        'Bridge': False, 
+                        'Router': False, 
+                        'Wlan': False, 
+                        'Station': False
+                    }
 
-        return info
+                    if ("capability" in chassis_info) and chassis_info['capability']:
+                        for capabilities in chassis_info['capability']:
+                            temp_dict['capability'][capabilities['type']] = capabilities['enabled']
 
-    except Exception as e:
-        #code
-        print(type(e))
-        print(f"Error running command: {e}")
+                    info['neighbor'].append(temp_dict)
+        except Exception as e:
+            #code
+            print(type(e))
+            print(f"Error running command: {e}")
+        finally:
+            return info
 
-def api_call(data):
-    """
-    API POST request to server to store the lldp data
+    def _make_api_request(self, data):
+        """
+        Makes a POST request to the server with the provided data.
 
-    Parameters:
-    - data: Dictionary that contains the data of chassis and neighbors 
+        Args:
+            data: The data to be sent to the server (dictionary).
 
-    Returns:
-    - HTTP response from the server
-    """
-    try:
-        # Code
-        host = "postman-echo.com"
-        endpoint = "/post"
+        Returns:
+            The JSON response from the server, or None if an error occurs.
+        """
+        try:
+            headers = {"Content-type": "application/json; charset=UTF-8"}
+            connection = http.client.HTTPSConnection(self.server_url)
 
-        post_data = json.dumps(data)
+            post_data = json.dumps(data)
+            connection.request("POST", self.api_endpoint, body=post_data, headers=headers)
 
-        headers = {
-            "Content-type": "application/json; charset=UTF-8"
-        }
+            response = connection.getresponse()
+            data = response.read().decode("utf-8")
+            json_data = json.loads(data)
+            connection.close()
 
-        connection = http.client.HTTPSConnection(host)
+            return json_data
+        except Exception as e:
+            print(f"An error occurred in the API call: {e}")
+            return None
 
-        connection.request("POST", endpoint, body=post_data, headers=headers)
+    def collect_and_send(self):
+        """
+        Collects LLDP information, creates a dictionary, and sends it to the server.
 
-        response = connection.getresponse()
+        Returns:
+            The server's response data as a dictionary, or None if an error occurs.
+        """
 
-        data = response.read()
+        chassis_output = self.run_lldpcli_command(self.command_chassis)
+        neighbor_output = self.run_lldpcli_command(self.command_neighbors)
 
-        decode_data = data.decode("utf-8")
+        if chassis_output and neighbor_output:
+            json_chassis = json.loads(chassis_output)
+            json_neighbors = json.loads(neighbor_output)
+            data = self.create_dict(json_chassis, json_neighbors)
+            return self._make_api_request(data)
 
-        json_data = json.loads(decode_data)
-
-        connection.close()
-
-        return json_data
-    except Exception as e:
-        print(f"An error has occured in API call: {e}")
-
-# Command to run local chassis information
-command_chassis = "sudo lldpcli -f json0 show chassis"
-
-# Command to run neighbor information
-command_neighbors = "sudo lldpcli -f json0 show neighbors"
-
-# Run the command and get the output
-output_chassis = run_lldpcli_command(command_chassis)
-output_neighbors = run_lldpcli_command(command_neighbors)
+        return None
 
 
-if output_chassis:
-    json_output_chassis = json.loads(output_chassis)
-    json_output_neighbors = json.loads(output_neighbors)
-    api_request = create_dict(json_output_chassis, json_output_neighbors)
-    api_response = api_call(api_request)
-    print("here", api_response['data'])
+# Example usage
+collector = LldpCollector("postman-echo.com", "/post")
+response_data = collector.collect_and_send()
+
+if response_data:
+    print("API response data:", response_data['data'])
